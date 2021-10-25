6dcf19f68a6e83ad7a69a862b53067c9b518caed
moratomicro <moratomicro@yahoo.com.br>
2021-10-25 18:58:33 -0300

    Criação de uma array e exibição dos dados do array na tela

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,37 @@
 import { Component } from "@angular/core";
+import { Course } from "./course";
 
 @Component ({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'November, 2, 2021'
+            },
+            {
+                id: 1,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-1094',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November, 4, 2021'
+            }
+        ]
+    }
     
 }
