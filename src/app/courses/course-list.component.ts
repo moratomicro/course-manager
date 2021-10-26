7ad495223e660e274b4a6056f1126d70807faa3b
moratomicro <moratomicro@yahoo.com.br>
2021-10-26 14:06:21 -0300

    Formatando dados do form com service e pipe

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component ({
     selector: 'app-course-list',
@@ -7,31 +8,26 @@ import { Course } from "./course";
 })
 export class CourseListComponent {
 
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+
+    _filterBy!: string;
+
+    constructor(private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'November, 2, 2021'
-            },
-            {
-                id: 1,
-                name: 'Angular: HTTP',
-                imageUrl: '',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'November, 4, 2021'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();      
+        this.filteredCourses = this._courses;  
     }
     
+    set filter(value: string) {
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
+    }
 }
